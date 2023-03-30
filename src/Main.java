a39af984be149090e9aa1d071e0d116d3373255f
muzaslivka <ms.dmitrieffa@gmail.com>
2023-03-30 20:08:05 +0300

    sort age lambda added

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,14 @@ public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
         people.add(new Person("Василий", "Петров", 16));
-        people.add(new Person("Мирослав", "Иванов Петров Сидоров", 12));
-        people.add(new Person("Екатерина", "Королёва Остапенко", 20));
-        people.add(new Person("Виктор", "Певцов Некрасов", 23));
-        people.add(new Person("Владислав", "Громов", 25));
+        people.add(new Person("Мирослав", "Иванов", 12));
+        people.add(new Person("Екатерина", "Королёва-Остапенко", 25));
+        people.add(new Person("Виктор", "Певцов-Некрасов", 23));
+        people.add(new Person("Владислав", "Громов-Некрасов-Нечепуренко", 25));
 
-        System.out.println("Неотсортированный список:");
-        System.out.println(people);
-        System.out.println();
+        people.removeIf(person -> person.getAge() < 18);
 
-        System.out.println("Отсортированный список:");
-        Collections.sort(people, new PersonComparator(3));
-        System.out.println(people);
+        people.forEach(System.out::println);
+
     }
 }
